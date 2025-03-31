e9a0205315626dd29a1a40add595d4a192e99ed0
samiam-44 <uriarte.samantha101@gmail.com>
2025-03-31 12:18:34 -0600

    files set up and started

---
+++ Scripts/index.js
@@ -0,0 +1,7 @@
+// cache elements
+const submitBtn = document.getElementById('submit-btn');
+const birthdateInput = document.getElementById('birthdate');
+const resultElement = document.getElementById('result');
+const lifePathInfo = document.querySelector('#lifePathInfo');
+const instructions = document.querySelector('#instructions');
+
