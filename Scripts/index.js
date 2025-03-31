5d75efbcbaa12b8bf98ef1e28675353e5f836e65
samiam-44 <uriarte.samantha101@gmail.com>
2025-03-31 12:30:59 -0600

    Event listener done

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,3 +5,13 @@ const resultElement = document.getElementById('result');
 const lifePathInfo = document.querySelector('#lifePathInfo');
 const instructions = document.querySelector('#instructions');
 
+//Event listener for info submition
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault(); //prevents page reload
+    const birthdate = birthdateInput.value
+
+    if (!birthdate) {
+        alert('Please enter a valid date')
+        return;
+    }
+})
